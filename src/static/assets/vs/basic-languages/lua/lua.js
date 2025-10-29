5d9c3c180f2491405aa73efb9813d02ea2d9ef16
Luka <164436043+ghluka@users.noreply.github.com>
2025-10-29 02:27:42 -0400

    luau auto closing pairs and a little highlighting

--- src/static/assets/vs/basic-languages/lua/lua.js
+++ src/static/assets/vs/basic-languages/lua/lua.js
@@ -16,129 +16,144 @@ define(["require", "exports"], function (require, exports) {
 			['(', ')'],
 		],
 		autoClosingPairs: [
-			{open: '{', close: '}'},
-			{open: '[', close: ']'},
-			{open: '(', close: ')'},
-			{open: '"', close: '"'},
-			{open: "'", close: "'"},
+			{ open: '{', close: '}' },
+			{ open: '[', close: ']' },
+			{ open: '(', close: ')' },
+			{ open: '"', close: '"' },
+			{ open: "'", close: "'" },
+			{ open: '`', close: '`' },
 		],
 		surroundingPairs: [
-			{open: '{', close: '}'},
-			{open: '[', close: ']'},
-			{open: '(', close: ')'},
-			{open: '"', close: '"'},
-			{open: "'", close: "'"},
+			{ open: '{', close: '}' },
+			{ open: '[', close: ']' },
+			{ open: '(', close: ')' },
+			{ open: '"', close: '"' },
+			{ open: "'", close: "'" },
+			{ open: '`', close: '`' },
 		],
 		folding: {
 			markers: {
-				start: new RegExp("^\\s*//\\s*(?:(?:#?region\\b)|(?:<editor-fold\\b))"),
-				end: new RegExp("^\\s*//\\s*(?:(?:#?endregion\\b)|(?:</editor-fold>))")
+				start: new RegExp("^\\s*--\\s*(?:(?:#?region\\b)|(?:<editor-fold\\b))"),
+				end: new RegExp("^\\s*--\\s*(?:(?:#?endregion\\b)|(?:</editor-fold>))")
 			}
 		},
 	};
+
 	exports.language = {
 		defaultToken: '',
-		tokenPostfix: '.lua',
+		tokenPostfix: '.luau',
+
 		keywords: [
-			// https://developer.roblox.com/en-us/articles/Variables
 			'and', 'break', 'continue', 'do', 'else', 'elseif',
-			'end', 'false', 'for', 'function', 'if',
-			'in', 'local', 'nil', 'not', 'or',
-			'repeat', 'return', 'then', 'true', 'until',
-			'while',
+			'end', 'false', 'for', 'function', 'if', 'in',
+			'local', 'nil', 'not', 'or', 'repeat', 'return',
+			'then', 'true', 'until', 'while',
+
+			'export', 'type', 'typeof', 'declare',
 		],
+
 		brackets: [
 			{ token: 'delimiter.bracket', open: '{', close: '}' },
 			{ token: 'delimiter.array', open: '[', close: ']' },
 			{ token: 'delimiter.parenthesis', open: '(', close: ')' }
 		],
+
 		globals: [
-			'print', 'error', 'warn', 'require', 'game', 'assert',
-
-			'rawset', 'rawget', 'rawequal',
-
-			'coroutine', 'debug', 'table', 'pairs', 'ipairs', 'bit', 'bit32', 'math', 'utf8', 'os',
-
-			'collectgarbage', 'getfenv', 'getmetatable',
-			'loadfile', 'loadstring', 'newproxy', 'next',
-			'pcall', 'select', 'setfenv',
-			'setmetatable', 'tonumber', 'tostring', 'type', 'xpcall', '_G',
-			'shared', 'delay', 'spawn', 'tick', 'typeof', 'wait',
-			'Enum', 'script', 'workspace', 'printidentity',
+			'print', 'warn', 'error', 'require', 'game', 'workspace', 'script',
+			'assert', 'pcall', 'xpcall', 'typeof', 'type', 'task', 'table', 'math',
+			'string', 'os', 'utf8', 'bit32', 'coroutine',
+			'spawn', 'delay', 'wait', 'tick', 'time', 'collectgarbage',
+			'Instance', 'Vector3', 'CFrame', 'Color3', 'Enum', '_G',
 		],
+
 		types: [
+			'number', 'string', 'boolean', 'nil', 'any', 'unknown',
+			'thread', 'userdata', 'table', 'function', 'never',
+
 			'Axes', 'BrickColor', 'CatalogSearchParams', 'CFrame', 'Color3', 'ColorSequence',
-			'ColorSequenceKeypoint', 'DateTime', 'Drawing', 'DockWidgetPluginGuiInfo', 'Faces',
-			'Instance', 'NumberRange', 'NumberSequence', 'NumberSequenceKeypoint', 'PathWaypoint',
+			'ColorSequenceKeypoint', 'DateTime', 'DockWidgetPluginGuiInfo', 'Faces', 'Instance',
+			'NumberRange', 'NumberSequence', 'NumberSequenceKeypoint', 'PathWaypoint',
 			'PhysicalProperties', 'Random', 'Ray', 'RaycastParams', 'RaycastResult', 'Rect',
 			'Region3', 'Region3int16', 'TweenInfo', 'UDim', 'UDim2', 'Vector2', 'Vector2int16',
 			'Vector3', 'Vector3int16',
 		],
+
 		operators: [
 			'+', '-', '*', '/', '%', '^', '#', '..',
 			'==', '~=', '<=', '>=', '<', '>', 'or', 'and', 'not',
-			'=', '+=', '-=', '*=', '/=', '%=', '^=', "..=",
+			'=', '+=', '-=', '*=', '/=', '%=', '^=', '..=',
 		],
 
 		symbols: /[=><!~?:&|+\-*\/\^%]+/,
-		escapes: /\\(?:[abfnrtv\\"']|x[0-9A-Fa-f]{1,4}|u[0-9A-Fa-f]{4}|U[0-9A-Fa-f]{8})/,
+		escapes: /\\(?:[abfnrtv\\"'`]|x[0-9A-Fa-f]{1,4})/,
 
 		tokenizer: {
 			root: [
 				[/[a-zA-Z_]\w*/, {
-						cases: {
-							'@keywords': { token: 'keyword.$0' },
-							'@globals': { token: 'global' },
-							'@types': { token: 'type' },
-							'@default': 'identifier'
-						}
-					}],
+					cases: {
+						'@keywords': { token: 'keyword.$0' },
+						'@globals': { token: 'global' },
+						'@types': { token: 'type' },
+						'@default': 'identifier'
+					}
+				}],
+
 				{ include: '@whitespace' },
+
 				[/(,)(\s*)([a-zA-Z_]\w*)(\s*)(:)(?!:)/, ['delimiter', '', 'key', '', 'delimiter']],
 				[/({)(\s*)([a-zA-Z_]\w*)(\s*)(:)(?!:)/, ['@brackets', '', 'key', '', 'delimiter']],
+
 				[/[{}()\[\]]/, '@brackets'],
+
 				[/@symbols/, {
-						cases: {
-							'@operators': 'delimiter',
-							'@default': ''
-						}
-					}],
+					cases: {
+						'@operators': 'delimiter',
+						'@default': ''
+					}
+				}],
+
 				[/\d*\.\d+([eE][\-+]?\d+)?/, 'number.float'],
 				[/0[xX][0-9a-fA-F_]*[0-9a-fA-F]/, 'number.hex'],
-				[/\d+?/, 'number'],
+				[/\d+/, 'number'],
+
 				[/[;,.]/, 'delimiter'],
+
 				[/"([^"\\]|\\.)*$/, 'string.invalid'],
 				[/'([^'\\]|\\.)*$/, 'string.invalid'],
+				[/`([^`\\]|\\.)*$/, 'string.invalid'],
 				[/"/, 'string', '@string."'],
 				[/'/, 'string', '@string.\''],
+				[/`/, 'string', '@string.`'],
 			],
+
 			whitespace: [
 				[/[ \t\r\n]+/, ''],
 				[/--\[([=]*)\[/, 'comment', '@comment.$1'],
 				[/--[tT][oO][-]?[dD][oO].*$/, 'comment.todo'],
 				[/--.*$/, 'comment'],
 			],
+
 			comment: [
 				[/[^\]]+/, 'comment'],
 				[/\]([=]*)\]/, {
-						cases: {
-							'$1==$S2': { token: 'comment', next: '@pop' },
-							'@default': 'comment'
-						}
-					}],
+					cases: {
+						'$1==$S2': { token: 'comment', next: '@pop' },
+						'@default': 'comment'
+					}
+				}],
 				[/./, 'comment']
 			],
+
 			string: [
-				[/[^\\"']+/, 'string'],
+				[/[^\\"'`]+/, 'string'],
 				[/@escapes/, 'string.escape'],
 				[/\\./, 'string.escape.invalid'],
-				[/["']/, {
-						cases: {
-							'$#==$S2': { token: 'string', next: '@pop' },
-							'@default': 'string'
-						}
+				[/["'`]/, {
+					cases: {
+						'$#==$S2': { token: 'string', next: '@pop' },
+						'@default': 'string'
 					}
-				]
+				}]
 			],
 		},
 	};
